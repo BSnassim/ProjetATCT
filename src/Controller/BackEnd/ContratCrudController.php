a9e6e67dfda339afeffd2dd0d9cb990f9acee1d2
BSnassim <ghost.bs2000@gmail.com>
2022-01-26 12:25:04 +0100

    wip 7

--- src/Controller/BackEnd/ContratCrudController.php
+++ src/Controller/BackEnd/ContratCrudController.php
@@ -4,6 +4,13 @@ namespace App\Controller\BackEnd;
 
 use App\Entity\Contrat;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+
 
 class ContratCrudController extends AbstractCrudController
 {
@@ -12,14 +19,33 @@ class ContratCrudController extends AbstractCrudController
         return Contrat::class;
     }
 
-    /*
+    
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
+            AssociationField::new('numFournisseur','Fournisseur'),
+            DateField::new('dateDebut'),
+            DateField::new('dateFin'),
+            IntegerField::new('preavis'),
+            ChoiceField::new('type')
+            ->setChoices([
+                'Tacite reconduction ouvert'=>'Tacite reconduction ouvert',
+                'Ferme'=>'Ferme',
+                'Tacite 1 an + 2 ans'=>'Tacite 1 an + 2 ans',
+            ]),
+            MoneyField::new('montant')->setCurrency('TND'),
+            IntegerField::new('numEnregistrement'),
+            TextField::new('periodiciteEntretien'),
+            ChoiceField::new('periodiciteFacturation')
+            ->setChoices([
+                'Mensuel'=>'Mensuel',
+                'Trimestriel'=>'Trimestriel',
+                'Semestriel'=>'Semestriel',
+                'Annuel'=>'Annuel',
+            ])
+            ->renderExpanded(),
+            
         ];
     }
-    */
+    
 }
