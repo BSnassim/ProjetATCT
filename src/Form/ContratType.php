2174c77d9fc02ecd7a7779c7c4b7f685b8484358
BSnassim <ghost.bs2000@gmail.com>
2022-01-27 00:14:08 +0100

    form types

--- src/Form/ContratType.php
+++ src/Form/ContratType.php
@@ -7,25 +7,31 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Vich\UploaderBundle\Form\Type\VichFileType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ContratType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('numFournisseur')
+            ->add('objet')
             ->add('dateDebut')
             ->add('dateFin')
             ->add('preavis')
-            ->add('type')
+            ->add('type', ChoiceType::class, array(
+                'choices' => array(
+                    '1'=>'Uns',
+                    '2'=>'Deux',
+                    '3'=>'Trois',
+                ),))
             ->add('montant')
             ->add('numEnregistrement')
             ->add('periodiciteEntretien')
             ->add('periodiciteFacturation')
             ->add('augmentation')
-            
-            ->add('filePDF',VichFileType::class)
-            ->add('numFournisseur')
-        ;
+            ->add('filePDF',VichFileType::class, ['label' => 'PDF'])
+            ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
