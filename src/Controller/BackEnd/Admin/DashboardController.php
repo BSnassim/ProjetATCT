983517a735bbde58d80ca2f8820d5de6912ff251
BSnassim <ghost.bs2000@gmail.com>
2022-01-31 12:45:09 +0100

    translated some things

--- src/Controller/BackEnd/Admin/DashboardController.php
+++ src/Controller/BackEnd/Admin/DashboardController.php
@@ -13,35 +13,21 @@ use App\Entity\Fournisseur;
 use App\Entity\TypesContrat;
 use App\Entity\User;
 use App\Entity\Admin;
+use Symfony\UX\Chartjs\Builder\ChartBuilderInterface;
+use Symfony\UX\Chartjs\Model\Chart;
 
 class DashboardController extends AbstractDashboardController
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-        //return parent::index();
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
-        //$adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
-        //return $this->redirect($adminUrlGenerator->setController(FournisseurCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        return $this->render('@EasyAdmin/page/content.html.twig');
+        return $this->render('admin/dashboard.html.twig');
     }
-
+    
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
-            ->setTitle('ProjetATCT');
+            ->setTitle('ATCT');
     }
 
     public function configureMenuItems(): iterable
